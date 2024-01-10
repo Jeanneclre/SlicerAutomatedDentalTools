e6aa5f11076a027914f7cf69cf61418b07d9b580
Jeanneclre <jeanne.claret@cpe.fr>
2024-01-10 12:00:24 -0500

    ENH: Build Debug

---
+++ SlicerAutomatedDentalTools-debug/AdditionalLauncherSettings-configure.cmake
@@ -0,0 +1,25 @@
+file(WRITE /home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/AdditionalLauncherSettings.ini
+"[General]
+additionalPathVariables=PYTHONPATH
+
+[LibraryPaths]
+1\\path=/home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/lib/Slicer-5.7/cli-modules/${CMAKE_CFG_INTDIR}
+2\\path=/home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/lib/Slicer-5.7/qt-loadable-modules/${CMAKE_CFG_INTDIR}
+size=2
+
+[Paths]
+1\\path=/home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/lib/Slicer-5.7/cli-modules/${CMAKE_CFG_INTDIR}
+2\\path=/home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/bin/${CMAKE_CFG_INTDIR}
+size=2
+
+[EnvironmentVariables]
+
+
+
+
+[PYTHONPATH]
+1\\path=/home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/lib/Slicer-5.7/qt-scripted-modules
+2\\path=/home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/lib/Slicer-5.7/qt-loadable-modules/${CMAKE_CFG_INTDIR}
+3\\path=/home/luciacev/Desktop/Jeanne_Claret/SlicerAutomatedDentalTools/SlicerAutomatedDentalTools-debug/lib/Slicer-5.7/qt-loadable-modules/Python
+size=3
+")
